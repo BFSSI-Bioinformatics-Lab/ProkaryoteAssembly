6f4772ea9245cbe495ff3e46805a37e60ae5a214
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-01-09 15:05:44 -0500

    0.1.6: Bug fix

--- ProkaryoteAssembly/prokaryote_assemble.py
+++ ProkaryoteAssembly/prokaryote_assemble.py
@@ -182,37 +182,38 @@ def call_tadpole(fwd_reads: Path, rev_reads: Path, out_dir: Path) -> tuple:
     return fwd_out, rev_out
 
 
-def bbduk_trim_adapters(fwd_reads: Path, rev_reads: Path, outdir: Path) -> tuple:
-    fwd_out = outdir / fwd_reads.name.replace(".fastq.gz", ".cleaned.fastq.gz")
-    rev_out = outdir / rev_reads.name.replace(".fastq.gz", ".cleaned.fastq.gz")
-    stats_out = outdir / 'adapter_trimming_stats.txt'
+def bbduk_trim_adapters(fwd_reads: Path, rev_reads: Path, out_dir: Path) -> tuple:
+    fwd_out = out_dir / fwd_reads.name.replace(".fastq.gz", ".cleaned.fastq.gz")
+    rev_out = out_dir / rev_reads.name.replace(".fastq.gz", ".cleaned.fastq.gz")
+    stats_out = out_dir / 'adapter_trimming_stats.txt'
     cmd = f"bbduk.sh in1={fwd_reads} in2={rev_reads} out1={fwd_out} out2={rev_out} " \
           f"ref=adapters tpe tbo overwrite=t unbgzip=f ktrim=r k=23 mink=11 hdist=1 stats={stats_out}"
     run_subprocess(cmd)
     return fwd_out, rev_out
 
 
-def bbduk_qc_filtering(fwd_reads: Path, rev_reads: Path, outdir: Path) -> tuple:
-    fwd_out = outdir / fwd_reads.name.replace(".fastq.gz", ".filtered.fastq.gz")
-    rev_out = outdir / rev_reads.name.replace(".fastq.gz", ".filtered.fastq.gz")
-    stats_out = outdir / 'quality_filtering_stats.txt'
+def bbduk_qc_filtering(fwd_reads: Path, rev_reads: Path, out_dir: Path) -> tuple:
+    fwd_out = out_dir / fwd_reads.name.replace(".fastq.gz", ".filtered.fastq.gz")
+    rev_out = out_dir / rev_reads.name.replace(".fastq.gz", ".filtered.fastq.gz")
+    stats_out = out_dir / 'quality_filtering_stats.txt'
     cmd = f"bbduk.sh in1={fwd_reads} in2={rev_reads} out1={fwd_out} out2={rev_out} unbgzip=f qtrim=rl " \
           f"trimq=10 2> {stats_out}"
     run_subprocess(cmd)
     return fwd_out, rev_out
 
 
-def call_bbduk(fwd_reads: Path, rev_reads: Path, outdir: Path) -> tuple:
+def call_bbduk(fwd_reads: Path, rev_reads: Path, out_dir: Path) -> tuple:
     """
     System call to bbduk.sh to perform adapter trimming/quality filtering on a given read pair
     :param fwd_reads: Path to forward reads (.fastq.gz)
     :param rev_reads: Path to reverse reads (.fastq.gz)
-    :param outdir: Path to desired output directory
+    :param out_dir: Path to desired output directory
     :return: tuple(filtered forward reads, filtered reverse reads)
     """
-    fwd_reads_trimmed, rev_reads_trimmed = bbduk_trim_adapters(fwd_reads=fwd_reads, rev_reads=rev_reads, outdir=outdir)
+    fwd_reads_trimmed, rev_reads_trimmed = bbduk_trim_adapters(fwd_reads=fwd_reads, rev_reads=rev_reads,
+                                                               out_dir=out_dir)
     fwd_reads_filtered, rev_reads_filtered = bbduk_qc_filtering(fwd_reads=fwd_reads_trimmed,
-                                                                rev_reads=rev_reads_trimmed, outdir=outdir)
+                                                                rev_reads=rev_reads_trimmed, out_dir=out_dir)
     return fwd_reads_filtered, rev_reads_filtered
 
 
